ccca2ce25764da1c76679f2bccd20ac96d53a482
Víctor Franco <victorfranco5@outlook.com>
2022-06-27 20:39:54 -0300

    feat: reservar ingresso a partir da index.html

--- js/index.js
+++ js/index.js
@@ -1,4 +1,40 @@
 const proxEventos = document.querySelector('#prox-eventos');
+const btnReserva = document.querySelector('#btn-reserva');
+const nomePessoa = document.querySelector('#nome-pessoa');
+const emailPessoa = document.querySelector('#email-pessoa');
+
+function reservarIngresso (idEvento){
+    const reserva = {
+        owner_name: nomePessoa.value,
+        owner_email: emailPessoa.value,
+        number_tickets: 1,
+        event_id: idEvento,
+    };
+
+    const options = {
+        method: 'POST',
+        headers: {
+        'Content-Type': 'application/json',
+        },
+        body: JSON.stringify(reserva),
+    };
+
+    fetch('https://xp41-soundgarden-api.herokuapp.com/bookings', options)
+        .then((resposta) => {
+            return resposta.json();
+        })
+        .then((reserva) => {
+            alert(`Parabéns ${reserva.owner_name} sua reserva foi efetuada!`);
+            window.location.replace('index.html');
+        })
+        .catch((erro) => {
+            console.log(erro);
+        })
+}
+
+function setId (idEvento){
+    btnReserva.setAttribute('onclick', `reservarIngresso("${idEvento}")`);
+}
 
 function mostrarEventos (evento){
     for (index = 0; index < 3; index++){
@@ -8,23 +44,26 @@ function mostrarEventos (evento){
         const h2 = document.createElement('h2');
         const h4 = document.createElement('h4');
         const p = document.createElement('p');
-        const a = document.createElement('a');
+        const button = document.createElement('button');
 
         article.classList.add('evento', 'card', 'p-5', 'm-3');
-        a.classList.add('btn', 'btn-primary');
+        button.classList.add('btn', 'btn-primary');
 
-        a.setAttribute('href', `reservas.html?id=${evento[index]._id}`)
+        button.setAttribute('type', 'button');
+        button.setAttribute('data-toggle', 'modal');
+        button.setAttribute('data-target', '#modal-reserva');
+        button.setAttribute('onclick', `setId("${evento[index]._id}")`);
 
         h2.innerText = `${evento[index].name} - ${dataEvento.getDate()}/${dataEvento.getMonth()}/${dataEvento.getFullYear()}`;
         h4.innerText = evento[index].attractions.join(',');
         p.innerText = evento[index].description;
-        a.innerText = 'reservar ingresso'
+        button.innerText = 'reservar ingresso';
 
         proxEventos.appendChild(article);
         article.appendChild(h2);
         article.appendChild(h4);
         article.appendChild(p);
-        article.appendChild(a);
+        article.appendChild(button);
     }
 }
 
