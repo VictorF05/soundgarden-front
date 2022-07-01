59b919f4bb9457f3a12541d1f5eb78c5f297eedc
Víctor Franco <victorfranco5@outlook.com>
2022-06-30 23:06:31 -0300

    fix: responsividade pagina de eventos

--- js/eventos.js
+++ js/eventos.js
@@ -45,9 +45,17 @@ function mostrarEventos (eventos){
         const h4 = document.createElement('h4');
         const p = document.createElement('p');
         const button = document.createElement('button');
+        const cardHeader = document.createElement('div');
+        const cardBody = document.createElement('div');
+        const cardFooter = document.createElement('div');
 
-        article.classList.add('evento', 'card', 'p-5', 'm-3');
+        article.classList.add('evento', 'card', 'text-center','p-4', 'p-sm-5', 'm-3', 'w-auto');
         button.classList.add('btn', 'btn-primary');
+        cardHeader.classList.add('card-header', 'bg-white', 'border-0', 'p-0', 'm-0');
+        cardBody.classList.add('card-body');
+        cardFooter.classList.add('card-footer', 'bg-white', 'border-0', 'p-0', 'm-0');
+        h4.classList.add('card-title');
+        p.classList.add('card-text');
 
         button.setAttribute('type', 'button');
         button.setAttribute('data-toggle', 'modal');
@@ -60,10 +68,13 @@ function mostrarEventos (eventos){
         button.innerText = 'reservar ingresso';
 
         todosEventos.appendChild(article);
-        article.appendChild(h2);
-        article.appendChild(h4);
-        article.appendChild(p);
-        article.appendChild(button);
+        article.appendChild(cardHeader);
+        article.appendChild(cardBody);
+        article.appendChild(cardFooter);
+        cardHeader.appendChild(h2);
+        cardBody.appendChild(h4);
+        cardBody.appendChild(p);
+        cardFooter.appendChild(button);
     });
 }
 
